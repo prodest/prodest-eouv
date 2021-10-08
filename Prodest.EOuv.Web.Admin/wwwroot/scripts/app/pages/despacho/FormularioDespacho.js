50aaf4313d14d987f7601889dfd55ef0e3525604
Fernando Silva <fernando.silva@prodest.es.gov.br>
2021-10-08 10:18:21 -0300

    Formulario de despacho de manifestação (parcial).

---
+++ Prodest.EOuv.Web.Admin/wwwroot/scripts/app/pages/despacho/FormularioDespacho.js
@@ -0,0 +1,19 @@
+﻿const DespachoForm = {
+
+    name: 'DespachoManifestacao',
+    template: '#template-despacho-form',
+
+    data() {
+        return {
+            titulo: 'Formulário de Despacho'
+        }
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    }
+};
---
+++ Prodest.EOuv.Web.Admin/wwwroot/scripts/app/pages/despacho/SelecaoDestinatarios.js
@@ -0,0 +1,19 @@
+﻿const SelecaoDestinatarios = {
+
+    name: 'SelecaoDestinatarios',
+    template: '#template-selecao-destinatarios',
+
+    data() {
+        return {
+            titulo: 'Seleção de Destinatários'
+        }
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    }
+};
